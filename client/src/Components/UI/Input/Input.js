244af7ef3847af4816261c35cf8b82191da60107
wcrozier12 <wcc8@nau.edu>
2018-02-01 13:48:41 -0700

    added input change handler and submit form handler

--- client/src/Components/UI/Input/Input.js
+++ client/src/Components/UI/Input/Input.js
@@ -6,17 +6,47 @@ const Input = (props) => {
 
   switch (props.elementType) {
     case ('input'):
-      inputElement = <input className='InputElement' {...props.elementConfig} value={props.value}/>;
+      inputElement = 
+          <input 
+          className='InputElement' 
+          {...props.elementConfig} 
+          value={props.value}
+          onChange={props.changed}/>;
       break;
     case ('textarea'):
-      inputElement = <textarea className='InputElement'{...props.elementConfig} value={props.value}/>
+      inputElement = 
+          <textarea 
+          className='InputElement'
+          {...props.elementConfig} 
+          value={props.value}
+          onChange={props.changed}/>
+      break;
+    case ('select'):
+      inputElement = (
+          <select
+          className='InputElement'
+          value={props.value}
+          onChange={props.changed}>
+          {props.elementConfig.options.map(option => {
+            return <option value={option.value} key={option.value}>
+                    {option.displayValue}
+                   </option>
+          })}
+          </select>
+      );
       break;
     default:
-      inputElement = <input className='InputElement' {...props.elementConfig} value={props.value}/>
+      inputElement = 
+          <input 
+            className='InputElement' 
+            {...props.elementConfig} 
+            value={props.value}
+            onChange={props.changed}/>
   }
   return (
     <div className='Input'>
-      <label className='Label'>{props.label}</label>
+      <label className='Label'
+      onChange={props.changed}>{props.label}</label>
       {inputElement}
     </div>
   )
--- client/src/Containers/Checkout/ContactData/ContactData.js
+++ client/src/Containers/Checkout/ContactData/ContactData.js
@@ -61,13 +61,18 @@ class ContactData extends Component {
     },
     loading: false
   }
-
+  
   orderHandler = (event) => {
     event.preventDefault();
     this.setState({loading: true})
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+    }
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price
+      price: this.props.price,
+      orderData: formData
     }
     axios.post('/orders.json', order)
     .then(response => {
@@ -76,6 +81,19 @@ class ContactData extends Component {
     }) 
     .catch(error => this.setState({loading: false}))
   }
+
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm})
+  }
+
   render() {
     const formElementsArray = [];
 
@@ -86,15 +104,16 @@ class ContactData extends Component {
       })
     }
     let form = (
-        <form>
+        <form onSubmit={this.orderHandler}>
           {formElementsArray.map(formElement => {
             return <Input 
                       key={formElement.id}
                       elementType={formElement.config.elementType}
                       elementConfig={formElement.config.elementConfig}
-                      value={formElement.config.value} />
+                      value={formElement.config.value}
+                      changed={(event) => this.inputChangedHandler(event, formElement.id)} />
           })}
-          <Button btnType='Success' clicked={this.orderHandler}> ORDER </Button>
+          <Button btnType='Success'> ORDER </Button>
         </form>);
     if (this.state.loading) {
       form = <Spinner />
